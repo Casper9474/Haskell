8f86a3bc56a85b4f97caec16896b319f787300b5
Casper9474 <146725742+Casper9474@users.noreply.github.com>
2024-03-14 22:19:07 +0100

    Chapter 1 - 2nd session

---
+++ Chapter 1/3/Main.hs
@@ -0,0 +1,11 @@
+module Main where
+
+makeGreeting salutation person =
+  salutation <> " " <> person
+  
+greetPerson = makeGreeting "Hello"
+
+enthusiasticGreeting salutation =
+  makeGreeting (salutation <> "!")
+ 
+main = print "no salutation to show yet"
